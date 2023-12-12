17a39db584df0f4efad3b9291302f87385504d67
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2023-12-12 14:50:42 +0100

    react project

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './pecking_animation.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>3 LITTLE BIRBS</h1>
+        <h6>
+          Under Construction.
+        </h6>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
