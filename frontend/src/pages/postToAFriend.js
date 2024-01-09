0a16b492354fd9ded97d91ca3a091b9e05a6b4c4
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-01-09 14:01:50 +0100

    friend names in dropdown

--- frontend/src/pages/postToAFriend.js
+++ frontend/src/pages/postToAFriend.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useEffect } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import {jwtDecode} from 'jwt-decode';
 
 //Styled Components
 import { Title } from "../styledComponents/title";
@@ -16,6 +18,10 @@ import { Boxtitle } from "../styledComponents/boxtitle";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { birbImages } from "../assets/birbs/birbsimgs";
 
+//Costum Hooks
+import useMongoDBUserData from "../costumHooks/useMongoDBUserData";
+import { getFriends } from "../costumHooks/getFriends";
+
 import {Select, MenuItem} from "@mui/material";
 
 export default function PostToAFriend() {
@@ -28,6 +34,47 @@ export default function PostToAFriend() {
     const location = useLocation();
     const { birbImageBirb1, birbImageBirb2, birbImageBirb3, goodthing1, goodthing2,goodthing3, message } = location.state;
  
+
+  const { userData, setUserData } = useMongoDBUserData([]);
+
+  useEffect(() => {
+    if (userData) {
+    }
+  }
+  , [userData]);
+
+  const token = localStorage.getItem("token");
+  const decodedToken = jwtDecode(token);
+
+  const user = userData.find((user) => user.id === decodedToken.id);
+
+  const [friends, setFriends] = React.useState([]);
+
+  useEffect(() => {
+    getFriends(decodedToken.id)
+      .then((friends) => {
+        setFriends(friends);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+
+  const [selectedFriend, setSelectedFriend] = useState("");
+  const [selectedFriendName, setSelectedFriendName] = useState("");
+
+  const handleFriendChange = (event) => {
+    const friendIndex = parseInt(event.target.value, 10);
+    setSelectedFriend(friendIndex);
+    console.log(friends[friendIndex].username);
+    setSelectedFriendName(friends[friendIndex].username);
+    console.log(selectedFriendName);
+
+
+  };
+  
+  
   return (
     <div>
       <Title>Send 3 little Birbs<br/>to a Friend</Title>
@@ -43,8 +90,8 @@ export default function PostToAFriend() {
       <Select 
   labelId="select-label"
   className="select"
-  value={value}
-  onChange={(event) => setValue(event.target.value)}
+  value={selectedFriend}
+  onChange={handleFriendChange}
   sx={{
     marginTop: "15px",
     height: 50,
@@ -63,18 +110,20 @@ export default function PostToAFriend() {
     },
   }}
 >
-  <MenuItem disabled value={0}>
+
+
+<MenuItem disabled value={null}>
     Choose a friend
   </MenuItem>
-  <MenuItem value={1}>Red</MenuItem>
-  <MenuItem value={2}>Black</MenuItem>
-  <MenuItem value={3}>Blue</MenuItem>
-  <MenuItem value={4}>Green</MenuItem>
-  <MenuItem value={5}>Yellow</MenuItem>
+  {friends.map((friend, index) => (
+    <MenuItem key={index} value={index}>
+      {friend.username}
+    </MenuItem>
+  ))}
 </Select>
 
       <MainContainer>
-        <Boxtitle>3 little Birbs for: NAME</Boxtitle>
+      <Boxtitle>3 little Birbs for: {selectedFriendName}</Boxtitle>
         <GoodThingContainerBirdLeft>
           <div>
             <img className="birdImg" src={birbImageBirb1} alt="hi"></img>
@@ -95,7 +144,7 @@ export default function PostToAFriend() {
           <InputGoodThing><textarea defaultValue={goodthing3}></textarea></InputGoodThing>
         </GoodThingContainerBirdLeft>
 
-        <Boxtitle>your message for NAME</Boxtitle>
+        <Boxtitle>your message for: {selectedFriendName}</Boxtitle>
 
         <HighlightedContainer>
         <InputGoodThing><textarea defaultValue={message}></textarea></InputGoodThing>
