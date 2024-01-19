34466c99053c14018ec29af626c065c08d52b2aa
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-01-19 14:09:44 +0100

    update

--- frontend/src/pages/notifications.js
+++ frontend/src/pages/notifications.js
@@ -63,7 +63,7 @@ export default function Notifications(props) {
       friendcode: friendcode,
     }).then(() => {
       setNotifications((currentNotifications) => currentNotifications.filter((notification) => notification.friendcode !== friendcode));
-      setTimeout(refetch, 1500);
+      refetch();
     });
   }
   
@@ -73,7 +73,7 @@ export default function Notifications(props) {
       postId: postId,
     }).then(() => {
       setNotifications((currentNotifications) => currentNotifications.filter((notification) => notification.id !== postId));
-     setTimeout(refetch, 1500);
+     refetch();
     });
   }
 
